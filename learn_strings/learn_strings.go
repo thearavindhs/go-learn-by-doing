baa2352bab8d13ece57c0cc1975310f9ee38fea3
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-08-26 13:27:28 -0400

    strings completion

--- learn_strings/learn_strings.go
+++ learn_strings/learn_strings.go
@@ -8,10 +8,14 @@ func main() {
 	// emtpy string of length 0, if value is not assigned its defaulted to 0
 	// var s string
 	// s = "Hello World!"
-	s := `"Hello, World!"`
+	s := `"Hello, World!"` // use string literal
 	fmt.Println(s)
+	b := s[2:6]
+	fmt.Println(b)
+	a := s[2]
+	fmt.Println(string(a)) // Go treats string as unmodifiable sequence of bytes
 	const y = "id"
 	fmt.Println(y)
-	y = "hello"
+	// y = "hello" // cant reassign constant
 
 }
